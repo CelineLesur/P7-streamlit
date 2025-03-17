9bc76542e66d0e458e3197c45d8c0056ac66eb85
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-03-17 21:16:38 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+rm -rf /tmp/*
+rm -rf /home/site/wwwroot/antenv
+
 echo "➡️  Activation de l'environnement virtuel..."
 if [ -d "/home/site/wwwroot/venv" ]; then
-    source /home/site/wwwroot/venv/bin/activate
-else
-    echo "⚠️  Environnement virtuel non trouvé ! Création..."
     python3 -m venv /home/site/wwwroot/venv
-    source /home/site/wwwroot/venv/bin/activate
-    echo "✅  Environnement virtuel créé."
 fi
+
+source /home/site/wwwroot/venv/bin/activate
 
 echo "➡️  Vérification de Python et pip..."
 which python
@@ -20,5 +20,5 @@ echo "➡️  Installation des dépendances..."
 pip install -r /home/site/wwwroot/requirements.txt
 
 echo "✅  Lancement de Streamlit sur le port 8501..."
-PORT=${PORT:-8501}
+PORT=${PORT:-8181}
 streamlit run /home/site/wwwroot/streamlit_app.py --server.port=$PORT --server.address=0.0.0.0
