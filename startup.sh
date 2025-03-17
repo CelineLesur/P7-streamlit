9ddd408630456c84ea3b94b04432ab9c87d2a682
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-03-17 20:29:06 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,60 +1,20 @@
 #!/bin/bash
 
-# 🔹 Vérifier si l'environnement virtuel existe
-if [ -d "/tmp/antenv" ]; then
-    echo "Activation de l'environnement virtuel existant..."
-    source /tmp/antenv/bin/activate
-else
-    echo "Création d'un nouvel environnement virtuel..."
-    python3 -m venv /tmp/antenv
-    source /tmp/antenv/bin/activate
-    pip install --upgrade pip
-    pip install -r /home/site/wwwroot/requirements.txt
+python3 -m venv /home/site/wwwroot/venv
+
+# Vérifier si l'environnement est Windows ou Linux/Mac
+if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "darwin"* ]]; then
+    # Si c'est Linux/Mac, activer l'environnement virtuel (Linux/Mac)
+    source /home/site/wwwroot/venv/bin/activate
+elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "win32" ]]; then
+    # Si c'est Windows, activer l'environnement virtuel avec activate.ps1 (PowerShell)
+    source /home/site/wwwroot/venv/Scripts/activate
 fi
 
-echo "Vérification de pip..."
-if ! command -v pip &> /dev/null; then
-    echo "pip non trouvé, installation en cours..."
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3 get-pip.py --user
-    export PATH=$HOME/.local/bin:$PATH
-    echo "pip installé avec succès."
-else
-    echo "pip est déjà installé."
-fi
+# Vérification de l'environnement virtuel
+echo "Environnement virtuel activé : $(which python)"
 
-echo "Installation des dépendances..."
 pip install --user -r /home/site/wwwroot/requirements.txt
 
-# 🔹 Lancer Streamlit (assurez-vous que `streamlit_app.py` est bien présent)
-echo "Démarrage de Streamlit..."
+# Démarrer l'application FastAPI
 streamlit run /home/site/wwwroot/streamlit_app.py --server.port=8501 --server.address=0.0.0.0
-
-# #!/bin/bash
-
-# # python3 -m venv /home/site/wwwroot/venv
-
-# # Extraire les fichiers du déploiement si nécessaire
-# if [ -f "/home/site/wwwroot/output.tar.gz" ]; then
-#     echo "Extraction de output.tar.gz..."
-#     tar -xvf /home/site/wwwroot/output.tar.gz -C /home/site/wwwroot/
-# fi
-
-# cd /home/site/wwwroot
-
-# # Vérifier si l'environnement est Windows ou Linux/Mac
-# if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "darwin"* ]]; then
-#     # Si c'est Linux/Mac, activer l'environnement virtuel (Linux/Mac)
-#     source antenv/bin/activate
-# elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "win32" ]]; then
-#     # Si c'est Windows, activer l'environnement virtuel avec activate.ps1 (PowerShell)
-#     source antenv/Scripts/activate
-# fi
-
-# # Vérification de l'environnement virtuel
-# echo "Environnement virtuel activé : $(which python)"
-
-# pip install -r requirements.txt
-
-# # Démarrer l'application FastAPI
-# streamlit run streamlit_app.py --server.port=8000 --server.address=0.0.0.0
