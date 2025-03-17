ad8bbbd5333f1f9673ba32b2221887cc48652c73
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-03-17 22:54:04 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -19,19 +19,6 @@ fi
 
 which pip3
 
-# echo "➡️  Activation de l'environnement virtuel..."
-# if [ -d "/home/site/wwwroot/venv" ]; then
-#     python3 -m venv /home/site/wwwroot/venv
-# fi
-
-# source /home/site/wwwroot/venv/bin/activate
-
-# echo "➡️  Vérification de Python et pip..."
-# which python
-# python --version
-# which pip
-# pip --version
-
 echo "➡️  Installation des dépendances..."
 pip3 install --no-cache-dir -r /home/site/wwwroot/requirements.txt
 
