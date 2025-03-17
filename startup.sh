c9dae9d69558859002db0cdaf5aae15d0f069c94
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-03-17 22:14:15 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,17 @@
 rm -rf /tmp/*
 rm -rf /home/site/wwwroot/antenv
 
+echo "Vérification de pip..."
+if ! command -v pip &> /dev/null; then
+    echo "pip non trouvé, installation en cours..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3 get-pip.py --user
+    export PATH=$HOME/.local/bin:$PATH
+    echo "pip installé avec succès."
+else
+    echo "pip est déjà installé."
+fi
+
 echo "➡️  Activation de l'environnement virtuel..."
 if [ -d "/home/site/wwwroot/venv" ]; then
     python3 -m venv /home/site/wwwroot/venv
@@ -17,7 +28,7 @@ which pip
 pip --version
 
 echo "➡️  Installation des dépendances..."
-pip install --user -r /home/site/wwwroot/requirements.txt
+pip install -r /home/site/wwwroot/requirements.txt
 
 echo "✅  Lancement de Streamlit sur le port 8181..."
 streamlit run /home/site/wwwroot/streamlit_app.py --server.port=8181 --server.address=0.0.0.0 > /home/site/wwwroot/logs.txt 2>&1 &
