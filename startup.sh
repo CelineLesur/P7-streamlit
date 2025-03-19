951d774f21340e1d119e525cca205e5fba2f3df8
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-03-19 14:34:53 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -32,5 +32,5 @@ pip3 install --no-cache-dir -r /home/site/wwwroot/requirements.txt
 
 echo "✅  Lancement de Streamlit sur le port 8501..."
 # streamlit run /home/site/wwwroot/streamlit_app.py --server.port=${PORT} --server.address=0.0.0.0 > /home/site/wwwroot/logs.txt 2>&1 &
-export STREAMLIT_PORT=8501
-python3 -m streamlit run /home/site/wwwroot/streamlit_app.py --server.port=8501 --server.address=0.0.0.0
+export PORT=8501
+python3 -m streamlit run /home/site/wwwroot/streamlit_app.py --server.port=$PORT --server.address=0.0.0.0
